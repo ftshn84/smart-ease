9eec53e8828687c5237adf8f8c0906b64bc9ee48
Fatima Sharifi <ftshn84@gmail.com>
2025-11-22 21:29:25 +0100

    edit names of variabels

--- speed.js
+++ speed.js
@@ -2,11 +2,12 @@ const travelInformation = {
   speed: 50,
   destinationDistance: 432,
 };
-function timeTravel(ti) {
-  const reTime = ti.destinationDistance / ti.speed;
-  const intPart = Math.floor(reTime);
-  const decPart = ((reTime - intPart) * 60).toFixed(0);
-  return intPart + " hours and " + decPart + " minutes";
+function timeTravel(travelInformation) {
+  const travelDuration =
+    travelInformation.destinationDistance / travelInformation.speed;
+  const travelHours = Math.floor(travelDuration);
+  const travelMinutes = ((travelDuration - travelHours) * 60).toFixed(0);
+  return travelHours + " hours and " + travelMinutes + " minutes";
 }
 const travelTime = timeTravel(travelInformation);
 console.log(travelTime); // 8 hours and 38 minutes
