a97e4a3b0af15362f393a1020f8aa5a1ba629c65
Fatima Sharifi <ftshn84@gmail.com>
2025-11-27 00:46:13 +0100

    error

--- voiceAssistant.js
+++ voiceAssistant.js
@@ -1,5 +1,5 @@
 const savedNames = new Set();
-const todo = [];
+const todos = [];
 let addRegex = /^add (.+) to my todo$/i;
 let removeRegex = /^remove (.+) from my todo$/i;
 let listRegex = /^what is on my todo\??$/i;
